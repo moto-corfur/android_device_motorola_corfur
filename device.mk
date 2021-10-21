23c5b700da61b076b86f6f0504ae2aaebeeef409
kubersharma001 <kubersharma001@gmail.com>
2021-10-21 15:41:57 +0530

    cofur: set adb secure 0 to eng builds
    Change-Id: I14ead6b4b25c10b5fa776779c78cafd9197a4b55

--- device.mk
+++ device.mk
@@ -50,6 +50,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
+# Properties
+include $(LOCAL_PATH)/configs/props/default.mk
+
 # Shipping level
 BOARD_API_LEVEL := 30
 BOARD_SHIPPING_API_LEVEL := 30
